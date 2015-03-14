760b276d3b47dc9eb3df679e0aca3d2923d130e8
Jiao Xiang <jxwr.cn@gmail.com>
2015-03-14 20:27:29 +0800

    ui: show ranges

--- public/app.js
+++ public/app.js
@@ -11,7 +11,68 @@ var RxNodeState = socket.map(function(e){ return JSON.parse(e.data); });
 
 /// react
 
-var NodeRow = React.createClass({
+/// NodeRangeState
+
+var NodeRangeBarItem = React.createClass({
+  render: function() {
+    var width = 1024;
+    var range = this.props.range;
+    var style = {
+      left: range.Left*width/16384,
+      width: (range.Right-range.Left+1)*width/16384,
+      backgroundColor: "green"
+    };
+    return (
+        <div className="nodeRangeBarItem" style={style}>
+        </div>
+    );
+  }
+});
+
+var NodeRangeRow = React.createClass({
+  render: function() {
+    var id = this.props.nodeid;
+    var ranges = this.props.ranges;
+    var items = ranges.map(function (range) {
+      return (
+          <NodeRangeBarItem range={range} />
+      );
+    });
+    return (
+      <tr className="nodeRow">
+        <td>{id.substring(0,6)}</td>
+        <td>
+          <div className="nodeRangeBar">
+          {items}
+          </div>
+        </td>
+      </tr>
+    );
+  }
+});
+
+var NodeRangeTable = React.createClass({
+  render: function() {
+    var nodes = this.props.nodes;
+    var keys = _.keys(this.props.nodes).filter(function(key){
+      return nodes[key].Role == "master";
+    }).sort();
+    var rows = keys.map(function (key) {
+      return (
+        <NodeRangeRow nodeid={nodes[key].Id} ranges={nodes[key].Ranges} />
+      );
+    });
+    return (
+      <table className="nodeTable">
+        {rows}
+      </table>
+    )
+  }
+});
+
+/// NodeStateTable
+
+var NodeStateRow = React.createClass({
   render: function() {
     var node = this.props.node;
     var FAIL = node.Fail ? "FAIL":"OK";
@@ -26,19 +87,34 @@ var NodeRow = React.createClass({
           <td>{WRITE}</td>
           <td>{node.Role}</td>
           <td>{node.Ip}:{node.Port}</td>
-          <td>{node.Id}</td>
+          <td>{node.Id.substring(0,6)}</td>
           <td>{node.Version}</td>
         </tr>
     );
   }
 });
 
-var NodeTable = React.createClass({
+var NodeStateTable = React.createClass({
+  render: function() {
+    var props = this.props;
+    var keys = _.keys(props.nodes).sort();
+    var nodes = keys.map(function (key) {
+      return (
+          <NodeStateRow node={props.nodes[key]} />
+      );
+    });
+    return (
+        <table className="nodeTable">
+          {nodes}
+        </table>
+    );
+  }
+});
+
+var Main = React.createClass({
   componentDidMount: function() {
     var self = this;
-    
     // 也许该用rx-react之类的Addon
-    // 目前逻辑太简单，丑就丑了
     RxNodeState.subscribe(
       function (obj) {
         var nodes = self.props.nodes;
@@ -53,22 +129,17 @@ var NodeTable = React.createClass({
       });
   },
   render: function() {
-    var props = this.props;
-    var keys = _.keys(props.nodes).sort();
-    var nodes = keys.map(function (key) {
-      return (
-          <NodeRow node={props.nodes[key]} />
-      );
-    });
+    var nodes = this.props.nodes;
     return (
-        <table className="nodeTable" nodes="nodes">
-          {nodes}
-        </table>
+      <div className="Main">
+        <NodeStateTable nodes={nodes} />
+        <NodeRangeTable nodes={nodes} />
+      </div>
     );
   }
 });
 
 React.render(
-    <NodeTable nodes={{}} />,
+    <Main nodes={{}} />,
     document.getElementById('content')
 );
